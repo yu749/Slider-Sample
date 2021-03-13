6b3e2154eccf4c3c4ff4d5f4c51465c6e281b1bc
yu <11111@gmail.com>
2021-03-13 18:06:26 +0900

    Initial Commit

---
+++ Slider Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider Sample
+//
+//  Created by Tanimura Yudai on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
